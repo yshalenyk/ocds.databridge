23bfbd0fc63aef83145addb12212d4a5aa9e1885
dimon.obert <dimon.obert@quintagroup.org>
2016-12-13 12:55:27 +0200

    added method for archivation packages

--- ocds/databridge/scripts/packages.py
+++ ocds/databridge/scripts/packages.py
@@ -9,6 +9,7 @@ from ocds.export.release import get_release_from_tender
 from uuid import uuid4
 import time
 import sys
+import tarfile
 
 
 URI = 'https://fake-url/tenders-{}'.format(uuid4().hex)
@@ -62,18 +63,25 @@ def dump_package(releases, config, dates=None):
             outfile.write(package.to_json())
 
 
+def compress_and_delete(source_dir):
+    path = os.path.join(source_dir, "releases{}".format(time.strftime("%Y-%m-%d")))
+    with tarfile.open(path, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+    for file in os.listdir(source_dir):
+        if file.endswith(".json"):
+            os.remove(os.path.join(source_dir, file))
+
+
 def run():
     args = parse_args()
     releases = []
     config = read_config(args.config)
     storage = TendersStorage(config.get('tenders_db'))
     info = config.get('release')
-
     if not os.path.exists(config.get('path_for_release_package')):
             os.makedirs(config.get('path_for_release_package'))
     if not os.path.exists(config.get('path_for_release_package_btw_dates')):
         os.makedirs(config.get('path_for_release_package_btw_dates'))
-
     if args.dates:
         datestart = parse_date(args.dates[0])
         datefinish = parse_date(args.dates[1])
@@ -97,3 +105,4 @@ def run():
                 dump_package(releases, config)
                 count = 0
                 releases = []
+    compress_and_delete(config.get('path_for_release_package'))
